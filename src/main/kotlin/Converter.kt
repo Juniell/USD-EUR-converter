6113c2bb722d348e0e4f220d05d1a00107b69956
Juniell <puchkina2000@mail.ru>
2021-12-11 03:50:18 +0300

    A converter from USD to EUR has been implemented, taking into account the received rate of these currencies.

--- src/main/kotlin/Converter.kt
+++ src/main/kotlin/Converter.kt
@@ -1,9 +1,12 @@
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.core.JacksonException
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import java.io.File
 import java.io.FileNotFoundException
 import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.net.ssl.HttpsURLConnection
 import kotlin.system.exitProcess
 
@@ -22,10 +25,23 @@ class Converter(private val amount: Int) {
         val url = URL("${baseUrl}?app_id=${token}&base=USD&symbols=EUR")
         val connection = url.openConnection() as HttpsURLConnection
         connection.requestMethod = "GET"
-
         val inputStream = connection.inputStream
-        val response: Response = jacksonObjectMapper().readValue(inputStream)
-        println("1 USD = ${response.rates["EUR"]} EUR")
+
+        try {
+            val response: Response = jacksonObjectMapper().readValue(inputStream)
+            printRes(response)
+        } catch (e: JacksonException) {
+            println("Ошибка при конвертации ответа от сервера.")
+            println(inputStream.bufferedReader().readText())
+            exitProcess(0)
+        }
+    }
+
+    private fun printRes(response: Response) {
+        val euro = amount * response.rates["EUR"]!!
+        val sdf = SimpleDateFormat("HH:mm dd.MM.yyyy")
+        val time = sdf.format(Date(response.timestamp * 1000))
+        println("$amount USD = $euro EUR (на $time)")
     }
 }
 
