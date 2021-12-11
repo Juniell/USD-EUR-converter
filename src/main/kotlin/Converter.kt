ae798a83fbc3ef4d1b3736c31a71f03d064fd406
Juniell <puchkina2000@mail.ru>
2021-12-11 03:33:21 +0300

    Implemented conversion of the received json into a class for the convenience of working with the data received from the service.

--- src/main/kotlin/Converter.kt
+++ src/main/kotlin/Converter.kt
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.io.File
 import java.io.FileNotFoundException
 import java.net.URL
@@ -8,19 +11,26 @@ class Converter(private val amount: Int) {
     private val baseUrl = "https://openexchangerates.org/api/latest.json"
     private val token: String =
         try {
-        val buffReader = File("token.txt").bufferedReader()
-        buffReader.readLine()
-    } catch (e: FileNotFoundException) {
-        println("The file \"token.txt\" with the token for openexchangerates.org was not found.")
-        exitProcess(0)
-    }
+            val buffReader = File("token.txt").bufferedReader()
+            buffReader.readLine()
+        } catch (e: FileNotFoundException) {
+            println("The file \"token.txt\" with the token for openexchangerates.org was not found.")
+            exitProcess(0)
+        }
 
     fun request() {
         val url = URL("${baseUrl}?app_id=${token}&base=USD&symbols=EUR")
         val connection = url.openConnection() as HttpsURLConnection
         connection.requestMethod = "GET"
 
-        val inpStream = connection.inputStream.bufferedReader()
-        println(inpStream.readText())
+        val inputStream = connection.inputStream
+        val response: Response = jacksonObjectMapper().readValue(inputStream)
+        println("1 USD = ${response.rates["EUR"]} EUR")
     }
 }
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Response(
+    val timestamp: Long,
+    val rates: Map<String, Double>
+)
